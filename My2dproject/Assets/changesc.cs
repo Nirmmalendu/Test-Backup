c53326c16ddd1e9d0ef915f20267eb081409cf22
Nirmalendu <khan.nupur@gmail.com>
2018-01-05 10:13:10 +0600

    CS comment change

---
+++ My2dproject/Assets/changesc.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changesc : MonoBehaviour {
+
+    public void scenechange()
+    {
+        
+        SceneManager.LoadScene("Finding");
+    }
+
+    public void scenechange2()
+    {
+        SceneManager.LoadScene("secondlevel");
+        Time.timeScale = 1;
+    }
+
+    public void secondlevelcng()
+    {
+        SceneManager.LoadScene("level2");
+        Time.timeScale = 1;
+    }
+
+    public void mainmenucng()
+    {
+        SceneManager.LoadScene("menu");
+        Time.timeScale = 1;
+    }
+
+    public void mainscene()
+    {
+        SceneManager.LoadScene("Finding");
+        Time.timeScale = 1;
+    }
+
+    public void mecng()
+    {
+        SceneManager.LoadScene("me");
+        Time.timeScale = 1;
+    }
+}
---
+++ My2dproject/Assets/hiddenscr.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class hiddenscr : MonoBehaviour {
+
+    //public GameObject animobj;
+    public AudioSource soundclip;
+    public AudioSource alart;
+    public AudioSource clap;
+    public AudioSource back;
+    private int count;
+    public Text countText;
+    public GameObject panel;
+    Animator anim;
+    //Use this for initialization
+    void Start () {
+        scorescr.scorevalue = 0;
+        count = 3;
+        countText.text = "Attempt Left :" + count.ToString();
+        panel.SetActive(false);
+        soundclip.Stop();
+        alart.Stop();
+        clap.Stop();
+        anim = GetComponent<Animator>();
+        anim.StopPlayback();
+
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint((Input.GetTouch(0).position)), Vector2.zero);
+
+        
+            if (hit.collider != null)
+            {
+
+
+                
+                
+                
+                //gameObject.animation["animationName"].wrapMode = WrapMode.Once;
+                soundclip.Play();
+                scorescr.scorevalue += 10;
+                Destroy(hit.transform.gameObject);
+                if (scorescr.scorevalue >= 30)
+                {                
+                    clap.Play();
+                    panel.SetActive(true);
+                    Time.timeScale = 0;
+                }
+            }
+
+                if (hit.collider == null)
+            {
+                alart.Play();
+                //Debug.Log("Fail");
+                count = count - 1;
+                countText.text = "Attempt Left :" + count.ToString();
+
+                if (count < 0 )
+                {
+                    
+                    SceneManager.LoadScene("gameover");        
+                }
+
+            }
+
+        }
+
+    }
+
+    public void khanamim()
+    {
+        anim.Play("New Animation");
+    }
+}
